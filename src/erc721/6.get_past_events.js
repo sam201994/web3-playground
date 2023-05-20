2a3fdbaa87b7d0de48001b52d8a0e58beb992176
Smriti Goel <smritigoel93@gmail.com>
2023-05-20 14:20:20 +0530

    get past events

--- src/erc721/6.get_past_events.js
+++ src/erc721/6.get_past_events.js
@@ -2,8 +2,6 @@ var ethers = require("ethers");
 require("dotenv").config({ path: require("find-config")(".env") });
 
 var url = process.env.PROVIDER_NETWORK;
-const address = process.env.PUBLIC_ADDRESS_1;
-const address2 = process.env.PUBLIC_ADDRESS_2;
 const ft721Address = process.env.FT721_CONTRACT_ADDRESS;
 
 var provider = new ethers.providers.JsonRpcProvider(url);
@@ -14,8 +12,6 @@ const artifact = require("../contracts/FT721.json");
 
 //Contract details
 const contract = new ethers.Contract(ft721Address, artifact.abi, wallet);
-const zeroAddress = ethers.constants.AddressZero;
-
 
 async function getAllPastEvents() {
   // Get the block number of the latest block
@@ -24,18 +20,17 @@ async function getAllPastEvents() {
   // Initialize an array to store all events
   const allEvents = [];
 
+  const blockNumber = await provider.getBlockNumber();
+
+
   // Iterate over the contract's ABI to find all event names
   for (const item of artifact.abi) {
     if (item.type === 'event') {
       const eventName = item.name;
-
-      // Set the block range to retrieve past events
-      const fromBlock = 0; // Start from block 0 (genesis block)
-      const toBlock = latestBlockNumber; // End at the latest block
-      console.log(typeof latestBlockNumber)
+   
 
       // Retrieve past events for the current event name
-      const events = await contract.queryFilter(eventName, latestBlockNumber-10000, toBlock);
+      const events = await contract.queryFilter(eventName, blockNumber, blockNumber+10000);
       console.log({events})
       // Add the events to the allEvents array
       allEvents.push(...events);
